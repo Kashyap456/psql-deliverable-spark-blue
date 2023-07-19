e69d62a5127e00c5f7ae4c771b9f16db9ec48752
Kashyap Chaturvedula <kashyapc@seas.upenn.edu>
2023-07-19 12:26:07 -0700

    deliverable

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,119 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
+const pg = require("pg");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+const pickTicket = async () => {
+  const client = new pg.Client({
+    //if you want the following to work on your own computer
+    //replace the fields with your information
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
+    user: "kashyapc",
+    database: "blue_deliv",
+    password: "",
+    port: 5432,
+  });
 
+  await client.connect();
 
-app.get('/', (req, res) => {
-    console.log('Message Sent!')
-    let num = Math.floor(getRandomArbitrary(1, 7));
-    res.send([num])
-})
+  const tableName = "lottery";
+  const queryText = `SELECT * FROM ${tableName};`;
 
+  const res = await client.query(queryText);
+  await client.query("DELETE FROM lottery;");
+  console.log(res);
+  if (res.rowCount > 0) {
+    const { rows } = res;
+    const row = rows[(rows.length * Math.random()) | 0];
+    return row.username;
+  }
 
-const port = 3256
-const host = 'localhost'
+  //close connection
+  await client.end();
+
+  return "<ERROR>";
+};
+
+const buyTicket = async (user) => {
+  const client = new pg.Client({
+    //if you want the following to work on your own computer
+    //replace the fields with your information
+
+    user: "kashyapc",
+    database: "blue_deliv",
+    password: "",
+    port: 5432,
+  });
+
+  await client.connect();
+
+  const tableName = "lottery";
+  const queryText = `INSERT INTO ${tableName} (username) VALUES ($1)`;
+  const values = [user];
+  const res = await client.query(queryText, values);
+  console.log(res);
+
+  //close connection
+  await client.end();
+
+  return res;
+};
+
+const createUser = async (user) => {
+  const client = new pg.Client({
+    //if you want the following to work on your own computer
+    //replace the fields with your information
+
+    user: "kashyapc",
+    database: "blue_deliv",
+    password: "",
+    port: 5432,
+  });
+
+  await client.connect();
+
+  const tableName = "users";
+  const queryText = `INSERT INTO ${tableName} (username) VALUES ($1) ON CONFLICT (username) DO NOTHING;`;
+  const values = [user];
+  const res = await client.query(queryText, values);
+  console.log(res);
+
+  //close connection
+  await client.end();
+
+  return user;
+};
+
+app.post("/login", async (req, res) => {
+  console.log(req);
+  const { user } = req.body;
+  const useres = await createUser(user);
+  console.log(useres);
+  res.status(200).send(user);
+});
+
+app.post("/buy", async (req, res) => {
+  const { user } = req.body;
+  const ticket = await buyTicket(user);
+  res.status(200).send("bought");
+});
+
+app.get("/pick", async (req, res) => {
+  const ticket = await pickTicket();
+  if (ticket === "<ERROR>") {
+    res.status(200).send("<ERROR>");
+  } else {
+    res.send(ticket);
+  }
+});
+
+const port = 3256;
+const host = "localhost";
 app.listen(port, host, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 /*
 To run -->  node index.js
--- src/App.js
+++ src/App.js
@@ -1,41 +1,74 @@
-import React, { useState } from 'react'
-import Axios from 'axios'
-import './index.css'
-import DiceCard from './components/DiceCard.js'
+import React, { useState } from "react";
+import axios from "axios";
+import "./index.css";
+import DiceCard from "./components/DiceCard.js";
+
+const login = async (user, setUser) => {
+  const res = await axios.post("http://localhost:3256/login", { user: user });
+  setUser(res.data);
+};
+
+const buy = async (user) => {
+  const res = await axios.post("http://localhost:3256/buy", { user: user });
+};
+
+const pick = async () => {
+  const res = await axios.get("http://localhost:3256/pick");
+  return res.data;
+};
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [user, setUser] = useState("");
+  const [pickText, setPick] = useState("");
 
-    function getDiceRoll() {
-        Axios.get('http://localhost:3256')
-        .then(function (response) {
-            console.log('response successfully received, response below')
-            console.log(response)
-
-            //add diceroll
-            //'...' is useful syntax for getting a deep copy of arrays, look it up!
-            setDiceRolls([...diceRolls, response.data[0]]);
-        }).catch(function (error) {
-            console.log('response unsusccessfully received, error below')
-            console.log(error)
-        }).finally(function (){
-            console.log("This part is always executed no matter what")
-        }) 
-    }
-    
-
-    const [diceRolls, setDiceRolls] = useState([]);
-    
-    return (
-        <div className='container'>
-            <h1>Dice Roll Website</h1>
-            <div className='button'>
-              <button
-              onClick={() => {getDiceRoll()}}
-              >Generate New Roll</button>
-            </div>
-            {
-              diceRolls.map((diceRoll) => DiceCard(diceRoll))
-            }
+  return (
+    <div className="container">
+      <h1>Lottery!</h1>
+      {!!!user && (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="user"
+            onChange={(e) => setInput(e.target.value)}
+          ></input>
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              await login(input, setUser);
+              setInput("");
+            }}
+          >
+            Login
+          </button>
         </div>
-    )
+      )}
+      {user && (
+        <div className="login">
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              await buy(user);
+            }}
+          >
+            Buy
+          </button>
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              const p = await pick();
+              if (p === "<ERROR>") {
+                setPick("Please buy at least one ticket");
+              } else {
+                setPick(`Winner: ${p}`);
+              }
+            }}
+          >
+            Pick
+          </button>
+        </div>
+      )}
+      {pickText && <h3 className="winner">{pickText}</h3>}
+    </div>
+  );
 }
